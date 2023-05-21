f1207b3ca06d215f9cc994807807d076c729af01
EntropyBall <everywhere.stretch@gmail.com>
2023-05-21 19:00:05 +0200

    Init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>header (photo) + title + qr code</header>
+      <footer>Centre d'int&eacute;r&ecirc;ts</footer>
+    </>
+  )
+}
+
+export default App
