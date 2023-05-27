c982b29f882b3b7d61220277d5fbb27bbbbbedb0
EntropyBall <everywhere.stretch@gmail.com>
2023-05-27 18:06:27 +0200

    F1 - sidebar

---
+++ src/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <aside>
+            <p>Nom prénom</p>
+            <img src="" alt="" />
+        </aside>
+    );
+};
+
+export default Sidebar;
